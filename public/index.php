8408ba31a8f31952475ecb5dc9d32038f71d9c19
Levon <chahestian@hotmail.com>
2018-06-04 18:08:52 -0300

    banco de dados básico implementado

--- public/index.php
+++ public/index.php
@@ -6,13 +6,22 @@ require '../vendor/autoload.php';
 header("Content-Type: application/json");
 $app = new \Slim\App;
 $app->map(['GET','POST'],'/hello/', function (Request $request, Response $response, array $args) {
-    
+    require_once("db.php");
     //$name = $args['name'];
+	/*
+	----TESTE JSON----
     $parsedBody = $request->getBody();
     $pedido = json_decode($parsedBody,true);
 	$texto = $pedido['nome'];
     $response->getBody()->write("$texto");
-    
+	
+    */
+	$id = 1;
+	$stmt = $pdo->prepare('SELECT * FROM autenticacao');
+	$stmt->execute([]);
+	while ($data = $stmt->fetch(PDO::FETCH_ASSOC)){
+		echo json_encode($data);
+	}
     return $response;
 });
 $app->run();
