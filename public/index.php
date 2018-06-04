874050ad6db846ea3ea089932e11470c692eb131
Levon <chahestian@hotmail.com>
2018-06-03 23:06:24 -0300

    tratando request com json

--- public/index.php
+++ public/index.php
@@ -3,12 +3,16 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-
+header("Content-Type: application/json");
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-
+$app->map(['GET','POST'],'/hello/', function (Request $request, Response $response, array $args) {
+    
+    //$name = $args['name'];
+    $parsedBody = $request->getBody();
+    $pedido = json_decode($parsedBody,true);
+	$texto = $pedido['nome'];
+    $response->getBody()->write("$texto");
+    
     return $response;
 });
 $app->run();
