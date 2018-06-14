bca9b10361aafcb6b755aba6743f56d8b04cd141
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-06-14 16:46:15 -0300

    API registra todos os dados do usuario

--- public/registrar.php
+++ public/registrar.php
@@ -6,21 +6,46 @@ if(!isset($_SESSION)) {
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../vendor/autoload.php';
-header("Content-Type: application/json");
+
 $app->map(['POST'],'/registrar/', function (Request $request, Response $response, array $args) {
     require_once("db.php");
 	$registro = json_decode($request->getBody(),true);
 	
-	$nome = $registro["nome"];
+	$nome = $registro["nome"]; //nome completo
 	$email = $registro["email"];
 	$senha = $registro["senha"];
 	$tipo = $registro["tipo"];
+	$cpf = $registro["cpf"];
+	$rg = $registro["rg"];
+	$tel = $registro["tel"];
 	
 	$query = $pdo->prepare('INSERT INTO autenticacao (nome,email,senha,tipo) SELECT * FROM ( SELECT ?,?,?,?) AS temp WHERE NOT EXISTS (SELECT email FROM autenticacao WHERE email=?)');
 	$query->execute([$nome,$email,$senha,$tipo,$email]);
 	
-	var_dump ($registro);
+	
+	//recuperando id do usuario 
+	
+	$query = $pdo->prepare('SELECT id_usuario FROM autenticacao WHERE email=?');
+	$query->execute([$email]);
+	$res = $query->fetch(PDO::FETCH_ASSOC);
+	
+	$id = $res["id_usuario"];
+	echo "id encontrado $id ";
+	
+	$query = $pdo->prepare('INSERT INTO cliente (nome_completo,cpf,id_usuario,rg,email_contato,telcontato) SELECT * FROM ( SELECT ?,?,?,?,?,?) AS temp WHERE NOT EXISTS (SELECT email_contato FROM cliente WHERE email_contato=?)');
+	//$query->execute([$nome,$cpf,$id,$rg,$email,$tel,$email]);
+	$query->execute([$nome,$cpf,$id,$rg,$email,$tel,$email]);
+	
+	echo "usuario criado";
+	
+	/*
+	INSERT INTO cliente (nome_completo,cpf,id_usuario,rg,email_contato,telcontato) 
+	SELECT * FROM ( SELECT 'Clodovaldo','1234567890','17','123456789','clodovaldo@yahoo.com.br
+	','969692424') AS temp 
+	WHERE NOT EXISTS (SELECT email_contato FROM cliente WHERE email_contato='clodovaldo@yahoo.com.br
+	*/
+	
+	
 	/*
 	INSERT INTO autenticacao (nome,email,senha,tipo) 
 	SELECT * FROM ( SELECT 'teste','teste@example.com','senhateste','inquilino') AS temp 
@@ -29,4 +54,7 @@ $app->map(['POST'],'/registrar/', function (Request $request, Response $response
 	
 	return $response;
 });
+
+
+
 ?>
