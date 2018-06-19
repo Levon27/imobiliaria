d7cadb4de7f9d671ef2394e59ec7aa27d485bbbc
Levon <chahestian@hotmail.com>
2018-06-19 17:47:04 -0300

    busca de imoveis por cidade e tipo implementada

--- public/procura_imovel.php
+++ public/procura_imovel.php
@@ -19,4 +19,31 @@ $app->map(['GET'],'/imovel/{id}', function (Request $request, Response $response
 	
 	return $response;
 });
+
+$app->map(['GET'],'/imovel/{tipo}/{cidade}', function (Request $request, Response $response, array $args) {
+	$n = 0;
+	require("db.php");
+	$cidade = $args['cidade'];
+	$cidade = str_replace("_"," ",$cidade);
+	$cidade = ucwords($cidade);
+	$tipo = $args['tipo'];
+	
+	echo "cidade: $cidade";
+	
+	$query = $pdo->prepare('SELECT * FROM imoveis WHERE cidade LIKE ? AND tipo LIKE ?');
+	$query->execute([$cidade,$tipo]);
+	
+	while ($data = $query->fetch(PDO::FETCH_ASSOC)){
+		
+		echo json_encode($data);
+		$n++;
+	}
+	
+	if ($n==0){
+		echo "nenhum imovel encontrado";
+	}
+	
+	return $response;
+});
+
 ?>
