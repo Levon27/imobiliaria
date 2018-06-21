b9e35d46b8f13e5233dcf42e2884f8ba6626e626
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-06-21 19:00:13 -0300

    registrar contrato melhorado

--- public/registrar_contrato.php
+++ public/registrar_contrato.php
@@ -6,17 +6,20 @@ $app->map(['POST'],'/registrar/contrato', function (Request $request, Response $
 	if (!(logado())){
 		return $response->withStatus(403); //usuario nao logado
 	}
+	if (!($_SESSION['tipo']=='inquilino'))
+		return $response->withStatus(403);
+	
 	include("db.php");
 	
 	$contrato = json_decode($request->getBody(),true);
 
 	$id_prop = $contrato["id_prop"];
-	$id_inq = $contrato["id_inq"];
-	$valor = $contrato["valor"];
+	$id_inq = $_SESSION["id"];
 	$id_imovel = $contrato["id_imovel"];
 	
-	$query = $pdo->prepare('INSERT INTO contrato (id_proprietario,id_inquilino,valor,id_imovel) VALUES (?,?,?,?)');
-	$query->execute([$id_prop,$id_inq,$valor,$id_imovel]);
+	if ($_SESSION['tipo']=='inquilino')
+	$query = $pdo->prepare('INSERT INTO contrato (id_proprietario,id_inquilino,valor,id_imovel) VALUES (?,?,(SELECT valor_imovel FROM imoveis WHERE id_imovel=?),?)');
+	$query->execute([$id_prop,$id_inq,$id_imovel,$id_imovel]);
 	
 	//echo "contrato registrado";
 	return $response->withStatus(201);
