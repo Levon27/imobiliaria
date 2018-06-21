273e7905e1c955733a7a7d01e16e1830bd45088a
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-06-20 22:18:16 -0300

    busca de contrato por proprpietario e inquilino implementado

--- public/procura_contrato.php
+++ public/procura_contrato.php
@@ -25,3 +25,50 @@ $app->map(['GET'],'/contrato/{id}', function (Request $request, Response $respon
 	
 	return $response;
 });
+
+$app->map(['GET'],'/contrato/inquilino/{id}', function (Request $request, Response $response, array $args) use ($app) {
+	if (!(logado())){
+		return $response->withStatus(403); //usuario nao logado
+	}
+	
+	require("db.php");
+	$id_inquilino = $args['id'];
+	
+	
+	$query = $pdo->prepare('SELECT * FROM contrato WHERE id_inquilino=?');
+	
+	$query->execute([$id_inquilino]);
+	
+	if ($contrato = $query->fetchAll(PDO::FETCH_ASSOC)){
+		return $response->withJson($contrato,200);
+	} else {
+		return $response->withStatus(404);
+	}
+		
+	
+	return $response;
+});
+
+$app->map(['GET'],'/contrato/proprietario/{id}', function (Request $request, Response $response, array $args) use ($app) {
+	if (!(logado())){
+		return $response->withStatus(403); //usuario nao logado
+	}
+	
+	require("db.php");
+	$id_prop = $args['id'];
+	
+	
+	$query = $pdo->prepare('SELECT * FROM contrato WHERE id_proprietario=?');
+	
+	$query->execute([$id_prop]);
+	
+	if ($contrato = $query->fetchAll(PDO::FETCH_ASSOC)){
+		return $response->withJson($contrato,200);
+	} else {
+		return $response->withStatus(404);
+	}
+		
+	
+	return $response;
+});
+
