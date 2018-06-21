d1f404638cb343d823f79dbaf3dd8a02eb0bc03d
LEVON TCHOLAKIAN CHAHESTIAN <14.01587-0@maua.br>
2018-06-21 14:41:18 -0300

    funcao de enviar email criada. Falta implementar na rota

---
+++ public/funcao_enviar_email.php
@@ -0,0 +1,42 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+function enviar_reclamacao($email,$nome,$mensagem,$reclamador,$endereco){
+	$mail = new PHPMailer(true);
+		try {
+			//Server settings
+			$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+			$mail->isSMTP();                                      // Set mailer to use SMTP
+			$mail->Host = 'smtp-mail.outlook.com';  // Specify main and backup SMTP servers
+			$mail->SMTPAuth = true;                               // Enable SMTP authentication
+			$mail->Username = 'imobimobiliaria@outlook.com';      // SMTP username
+			$mail->Password = 'Softwaremuda';                     // SMTP password
+			$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+			$mail->Port = 587;                                    // TCP port to connect to
+
+			//Recipients
+			$mail->setFrom('imobimobiliaria@outlook.com', 'Mailer');
+			$mail->addAddress($email, $nome);     // Add a recipient
+
+			//Attachments
+			//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+			//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+			//Content
+			$mail->isHTML(true);                                  // Set email format to HTML
+			$mail->Subject = 'Reclamacao de um inquilino';
+			$mail->Body    = 
+			"<p> Ola, $nome. </p>
+			<p> O inquilino $reclamador fez uma reclamacao com respeito ao seu imovel em $endereco : </p>
+			<p> 	$mensagem </p>
+			<p> Esperamos que resolva o problema. </p> 
+			<p> iMob, a imobiliaria do futuro. </p>";
+			
+
+			$mail->send();
+			echo 'Message has been sent';
+		} catch (Exception $e) {
+			echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+		}
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ if(!isset($_SESSION)) {
 }  
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 require '../vendor/autoload.php';
 header("Content-Type: application/json");
@@ -21,6 +23,7 @@ require_once("nao_logado.php");
 require_once("procura_contrato.php");
 require_once("altera_imovel.php");
 require_once("altera_contrato.php");
+require_once("funcao_enviar_email.php");
 define('ROOT_PATH',dirname(__FILE__));
 
 $app->map(['GET','POST'],'/hello', function (Request $request, Response $response, array $args) {
@@ -35,21 +38,56 @@ $app->map(['GET','POST'],'/hello', function (Request $request, Response $respons
 	$response->getBody()->write("$texto");
 	*/
 	
-	
+	/*
 	$id = 2;
 	$stmt = $pdo->prepare('SELECT * FROM autenticacao WHERE id_usuario=?');
 	$stmt->execute([$id]);
 	if ($data = $stmt->fetchAll(PDO::FETCH_ASSOC)){
 		echo json_encode($data);
 	}
-	 
+	*/	
 	
 	
-	
-	
-	
+	//---TESTE ENVIAR EMAIL---
+	/*
+	$mail = new PHPMailer(true);
+	try {
+		//Server settings
+		$mail->SMTPDebug = 2;                                 // Enable verbose debug output
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host = 'smtp-mail.outlook.com';  // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = 'imobimobiliaria@outlook.com';      // SMTP username
+		$mail->Password = 'Softwaremuda';                     // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = 587;                                    // TCP port to connect to
+
+		//Recipients
+		$mail->setFrom('imobimobiliaria@outlook.com', 'Mailer');
+		$mail->addAddress('raul.vaz@uol.com.br', 'Jonas das mamona');     // Add a recipient
+
+		//Attachments
+		//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+		//Content
+		$mail->isHTML(true);                                  // Set email format to HTML
+		$mail->Subject = 'mandar email funfo';
+		$mail->Body    = 'Seu guei';
 		
-	
+
+		$mail->send();
+		echo 'Message has been sent';
+	} catch (Exception $e) {
+		echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+	}
+	*/
+	$email = 'chahestian@hotmail.com';
+	$nome = 'Levon';
+	$reclamador = 'João';
+	$mensagem = 'Rato entalado na privada';
+	$endereco = 'Av. Paulista, 666';
+	enviar_reclamacao($email,$nome,$mensagem,$reclamador,$endereco);
 	return $response;
 	
 	echo " não deveria chegar aqui seu guei";
